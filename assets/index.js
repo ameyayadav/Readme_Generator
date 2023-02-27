c97628542226b1038d99e4d26a527ecf35e8d27e
Ameya Yadav <ameyayadav2@gmail.com>
2023-02-27 22:34:01 +0000

    updated index js file

--- assets/index.js
+++ assets/index.js
@@ -6,7 +6,16 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    "What is your project's title?", 
+    "Please provide a short description of your project:",
+    "Please provide installation instructions for your project:",
+    "Please provide usage information for your project:",
+    "Please choose a license for your project:",
+    "Please provide contribution guidelines for your project:",
+    "Please provide test instructions for your project:",
+    "What is your name?",
+    "What is your email address?",
+    "What is your GitHub username?",
 ];
 
 // function to write README file
