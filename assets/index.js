4c1505b7767aa2677faad5cd97229284b21dd390
Ameya Yadav <ameyayadav2@gmail.com>
2023-02-27 22:46:11 +0000

    created a function to gnerate a readme file format

--- assets/index.js
+++ assets/index.js
@@ -19,13 +19,77 @@ const questions = [
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+// function writeToFile(fileName, data) {
+// }
 
 // function to initialize program
 function init() {
-
+    inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: "What is your project's title?",
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please provide a short description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Please provide installation instructions for your project:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Please provide usage information for your project:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Please choose a license for your project:',
+      choices: ['MIT', 'GNU GPL v3', 'Apache License 2.0', 'ISC', 'BSD 2-Clause License'],
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Please provide contribution guidelines for your project:',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Please provide test instructions for your project:',
+    },
+    {
+      type: 'input',
+      name: 'name',
+      message: "What is your name?",
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "What is your email address?",
+    },
+    {
+      type: 'input',
+      name: 'username',
+      message: "What is your GitHub username?",
+    },
+  ])
+  .then((answers) => {
+    const readme = generateREADME(answers);
+    fs.writeFile('README.md', readme, (err) =>
+      err ? console.log(err) : console.log('Successfully generated README!')
+    );
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 }
+
+
 
 // function call to initialize program
 init();
