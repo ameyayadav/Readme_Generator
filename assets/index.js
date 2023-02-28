7cd2b15de6c0377ff01548e68613f787c34719c9
Ameya Yadav <ameyayadav2@gmail.com>
2023-02-28 23:26:10 +0000

    fixed markdown js project working without any bugs

--- assets/index.js
+++ assets/index.js
@@ -4,20 +4,20 @@ import fs from "fs";
 // const inquirer = require("inquirer");
 import inquirer from "inquirer";
 // const generateMarkdown = require("./utils/generateMarkdown");
-import generateMarkdown from "./utils/generateMarkdown"
+import generateMarkdown from "./utils/generateMarkdown.js"
 // array of questions for user
-const questions = [
-    "What is your project's title?", 
-    "Please provide a short description of your project:",
-    "Please provide installation instructions for your project:",
-    "Please provide usage information for your project:",
-    "Please choose a license for your project:",
-    "Please provide contribution guidelines for your project:",
-    "Please provide test instructions for your project:",
-    "What is your name?",
-    "What is your email address?",
-    "What is your GitHub username?",
-];
+// const questions = [
+//     "What is your project's title?", 
+//     "Please provide a short description of your project:",
+//     "Please provide installation instructions for your project:",
+//     "Please provide usage information for your project:",
+//     "Please choose a license for your project:",
+//     "Please provide contribution guidelines for your project:",
+//     "Please provide test instructions for your project:",
+//     "What is your name?",
+//     "What is your email address?",
+//     "What is your GitHub username?",
+// ];
 
 // function to write README file
 function writeToFile(fileName, data) {
--- assets/utils/generateMarkdown.js
+++ assets/utils/generateMarkdown.js
@@ -1,8 +1,49 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Description
 
-`;
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## License
+
+[![License: ${data.license}](https://img.shields.io/badge/License-${data.license}-blue.svg)](https://opensource.org/licenses/${data.license})
+
+This project is licensed under the ${data.license} license.
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+For any questions, please contact ${data.name} at ${data.email}.
+
+GitHub: [${data.username}](https://github.com/${data.username})`;
 }
 
-module.exports = generateMarkdown;
+// module.exports = {generateMarkdown};
+
+export default generateMarkdown
+
